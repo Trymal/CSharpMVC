aba22e1b0e4f752276a3c5df29595fc57a47085b
BuildTools <unconfigured@null.spigotmc.org>
2021-03-07 09:12:40 +0100

    MVC + WCF

---
+++ CSharpMVC/Controllers/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace CSharpMVC.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
