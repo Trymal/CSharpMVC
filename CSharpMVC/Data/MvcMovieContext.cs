ea497b17857f84a5817e686c16229cd000d65f05
BuildTools <unconfigured@null.spigotmc.org>
2021-03-10 19:57:26 +0100

    MVC Movie

---
+++ CSharpMVC/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CSharpMVC.Models;
+
+namespace CSharpMVC.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- CSharpMVC/Startup.cs
+++ CSharpMVC/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSharpMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSharpMVC
 {
@@ -24,6 +26,8 @@ namespace CSharpMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
